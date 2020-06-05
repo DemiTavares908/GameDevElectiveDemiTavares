aa16edb0439b2f7abe3c2a8ff52c07f1ed0beac5
DemiTavares908 <59691843+DemiTavares908@users.noreply.github.com>
2020-06-06 00:40:38 +0200

    LittleThrowCube script added

---
+++ GameDevElectiveDemiTavares/Assets/Scripts/LittleThrowCube.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LittleThrowCube : MonoBehaviour
+{
+    void FixedUpdate(){
+        transform.Translate(new Vector3(0f, 0f, 500 * Time.fixedDeltaTime), Space.Self);
+    }
+
+}
